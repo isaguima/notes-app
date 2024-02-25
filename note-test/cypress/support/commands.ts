f1849a5ffb8b5821a022fb39ba028723b3964ef3
Isadora Guimarães <isadora.guima@gmail.com>
2024-02-25 19:25:43 -0300

    style: prettify identation

--- note-test/cypress/support/commands.ts
+++ note-test/cypress/support/commands.ts
@@ -3,49 +3,49 @@
 const API_URL = "http://localhost:3000/notes";
 
 Cypress.Commands.add('getNotes', () => {
-    return cy.api({
-        method: 'GET',
-        url: API_URL,
-    })
+  return cy.api({
+    method: 'GET',
+    url: API_URL,
+  })
 })
 
 Cypress.Commands.add('getNoteById', (id) => {
-    return cy.api({
-        method: 'GET',
-        url: `${API_URL}/${id}`,
-    })
+  return cy.api({
+    method: 'GET',
+    url: `${API_URL}/${id}`,
+  })
 })
 
 Cypress.Commands.add('deleteNote', (id) => {
-    return cy.api({
-        failOnStatusCode: false,
-        method: 'DELETE',
-        url: `${API_URL}/${id}`,
-    })
+  return cy.api({
+    failOnStatusCode: false,
+    method: 'DELETE',
+    url: `${API_URL}/${id}`,
+  })
 })
 
 Cypress.Commands.add('deleteAllNotes', () => {
-    cy.getNotes().then( response => {
-        response.body.forEach((note) => {
-            cy.deleteNote(note._id);
-        })
+  cy.getNotes().then(response => {
+    response.body.forEach((note) => {
+      cy.deleteNote(note._id);
     })
+  })
 })
 
 Cypress.Commands.add('postNote', (note) => {
-    cy.request({
-        failOnStatusCode: false,
-        method: 'POST',
-        url: API_URL,
-        body: note
-    })
+  return cy.api({
+    failOnStatusCode: false,
+    method: 'POST',
+    url: API_URL,
+    body: note
+  })
 })
 
 Cypress.Commands.add('putNote', (id, note) => {
-    return cy.api({
-        failOnStatusCode: false,
-        method: 'PUT',
-        url: `${API_URL}/${id}`,
-        body: note
-    })
+  return cy.api({
+    failOnStatusCode: false,
+    method: 'PUT',
+    url: `${API_URL}/${id}`,
+    body: note
+  })
 })
--- note-test/cypress/support/e2e.ts
+++ note-test/cypress/support/e2e.ts
@@ -1,18 +1,26 @@
-import './commands'
-import 'cypress-plugin-api'
+import './commands';
+import 'cypress-plugin-api';
 
-type Note = { title: string, content: string};
-type APINote = Note & { _id: string, __v: string, error: string };
+type Note = {
+  title: string,
+  content: string
+};
+
+type APINote = Note & {
+  _id: string,
+  __v: string,
+  error: string
+};
 
 declare global {
-   namespace Cypress {
-      interface Chainable {
-         getNotes(): Chainable < Cypress.Response< Array< APINote > > >
-         getNoteById(id: string): Chainable < Cypress.Response< APINote > >
-         postNote(note: Note): Chainable < Cypress.Response< APINote > >
-         putNote(id: string, note: Note): Chainable < Cypress.Response< APINote > >
-         deleteNote(id: string): Chainable < Cypress.Response< { error: string } > >
-         deleteAllNotes(): Chainable < void >
-      }
-   }
+  namespace Cypress {
+    interface Chainable {
+      getNotes(): Chainable < Cypress.Response < Array < APINote > > >
+        getNoteById(id: string): Chainable < Cypress.Response < APINote > >
+        postNote(note: Note): Chainable < Cypress.Response < APINote > >
+        putNote(id: string, note: Note): Chainable < Cypress.Response < APINote > >
+        deleteNote(id: string): Chainable < Cypress.Response < { error: string } > >
+        deleteAllNotes(): Chainable < void >
+    }
+  }
 }
